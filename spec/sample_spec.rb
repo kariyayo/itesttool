a12d8b81046e888d57526a6de963b451b54b4194
bati11 <mail.bati11@gmail.com>
2013-03-24 12:27:42 +0900

    説明等を修正

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,132 +1,147 @@
 require './lib/itesttool'
 
-describe "Access to /index" do
-  _when { get "http://localhost:4567/index" }
+describe 'send GET request' do
+  _when { get 'http://localhost:4567/index' }
   _then {
-    res.code.should eq "200"
-    res.body.should eq "Hello world!"
+    # status code
+    res.code.should eq '200'
+
+    # response body
+    res.body.should eq 'Hello world!'
   }
 end
 
-describe "Access to /index.json" do
-  _when { get "http://localhost:4567/index.json", as_json }
+describe 'expectation for returned JSON' do
+  _when { get 'http://localhost:4567/index.json', as_json }
   _then {
-    # status code
-    res.code.should eq "200"
+    res.code.should eq '200'
 
     # json schema
-    res.body.should eq_schema_of "json_schema/hello.json"
+    res.body.should eq_schema_of 'json_schema/hello.json'
 
     # using jsonpath
-    res["$.team"].should eq ["ABC"]
-    res["$.members..name"].should eq ["Ichiro", "Jiro", "Saburo"]
-    res["$.members..age"].should include 32
-    res["$.members[::]"].should have(3).items
-    res["$.members[::]"].should have_at_most(3).items
-    res["$.members[::]"].should have_at_least(1).items
-    res["$.members..name"].should all_be_type_of :string
-    res["$.members..age"].should all_be_type_of :integer
-    res["$.members..age"].should all_be_gt 11
-    res["$.members..age"].should all_be_gt_eq 12
-    res["$.members..age"].should all_be_lt 33
-    res["$.members..age"].should all_be_lt_eq 32
-    res["$.members..age"].should be_sorted :desc
+    res['$.team'].should eq ['ABC']
+    res['$.members..name'].should eq ['Ichiro', 'Jiro', 'Saburo']
+    res['$.members..age'].should include 32
+    res['$.members[::]'].should have(3).items
+    res['$.members[::]'].should have_at_most(3).items
+    res['$.members[::]'].should have_at_least(1).items
+    res['$.members..name'].should all_be_type_of :string
+    res['$.members..age'].should all_be_type_of :integer
+    res['$.members..age'].should all_be_gt 11
+    res['$.members..age'].should all_be_gt_eq 12
+    res['$.members..age'].should all_be_lt 33
+    res['$.members..age'].should all_be_lt_eq 32
+    res['$.members..age'].should be_sorted :desc
   }
 end
 
-describe "Access to /index.xml" do
-  _when { get "http://localhost:4567/index.xml", as_xml }
+describe 'expectation for returned XML' do
+  _when { get 'http://localhost:4567/index.xml', as_xml }
   _then {
-    # status code
-    res.code.should eq "200"
+    res.code.should eq '200'
 
     # using xpath
-    res["/root/team/text()"].should eq ["ABC"]
-    res["/root/members//name/text()"].should eq ["Ichiro", "Jiro", "Saburo"]
-    res["/root/members//age/text()"].should include "32"
-    res["/root/members/*"].should have(3).items
-    res["/root/members/*"].should have_at_most(3).items
-    res["/root/members/*"].should have_at_least(1).items
-    res["/root/members//age/text()"].should all_be_gt 11
-    member_ages = res["/root/members//age/text()"]
-    member_ages.should all_be_gt_eq 10
-    member_ages.should all_be_lt 33
-    member_ages.should all_be_lt_eq 32
-    member_ages.should be_sorted :desc
-    res["/root/members/member/@order"].should be_sorted :asc
+    res['/root/team/text()'].should eq ['ABC']
+    res['/root/members//name/text()'].should eq ['Ichiro', 'Jiro', 'Saburo']
+    res['/root/members//age/text()'].should include '32'
+    res['/root/members/*'].should have(3).items
+    res['/root/members/*'].should have_at_most(3).items
+    res['/root/members/*'].should have_at_least(1).items
+    res['/root/members//age/text()'].should all_be_gt 11
+    member_ages = res['/root/members//age/text()']
+      member_ages.should all_be_gt_eq 10
+      member_ages.should all_be_lt 33
+      member_ages.should all_be_lt_eq 32
+      member_ages.should be_sorted :desc
+    res['/root/members/member/@order'].should be_sorted :asc
   }
 end
 
-describe "Access to /index.html" do
-  _when { get "http://localhost:4567/index.html", as_html, "referer" => "http://local.example.com", "user_agent" => "itesttool" }
+describe 'expectation for returned HTML' do
+  _when { get 'http://localhost:4567/index.html', as_html }
   _then {
-    # status code
-    res.code.should eq "200"
+    res.code.should eq '200'
 
-    # using xpath
-    res["title"].should eq ["Page Title!"]
-    res["h1#team"].should eq ["ABC"]
-    res[".member dd.name"].should eq ["Ichiro", "Jiro", "Saburo"]
-    res[".member dd.age"].should include "32"
-    res[".member"].should have(3).items
-    res[".member"].should have_at_most(3).items
-    res[".member"].should have_at_least(1).items
-    res[".member dd.age"].should all_be_gt 11
-    member_ages = res[".member dd.age"]
-    member_ages.should all_be_gt_eq 10
-    member_ages.should all_be_lt 33
-    member_ages.should all_be_lt_eq 32
-    member_ages.should be_sorted :desc
+    # using CSS selector
+    res['title'].should eq ['Page Title!']
+    res['h1#team'].should eq ['ABC']
+    res['.member dd.name'].should eq ['Ichiro', 'Jiro', 'Saburo']
+    res['.member dd.age'].should include '32'
+    res['.member'].should have(3).items
+    res['.member'].should have_at_most(3).items
+    res['.member'].should have_at_least(1).items
+    res['.member dd.age'].should all_be_gt 11
+    member_ages = res['.member dd.age']
+      member_ages.should all_be_gt_eq 10
+      member_ages.should all_be_lt 33
+      member_ages.should all_be_lt_eq 32
+      member_ages.should be_sorted :desc
   }
 end
 
-describe "Status 200 check" do
-  status_check [
-      'http://localhost:4567/index.json',
-      'http://localhost:4567/index'
-  ]
+describe 'set request headers' do
+  context 'specified request headers in argument of "get()"' do
+    _when { get 'http://localhost:4567/index.html', as_html, 'referer' => 'http://local.example.com', 'user_agent' => 'itesttool' }
+    _then {
+      res.code.should eq '200'
+    }
+  end
+  context 'specified request headers in "_given"' do
+    _given {
+      headers 'referer' => 'http://local.example.com',
+              'user_agent' => 'itesttool'
+    }
+    _when { get 'http://localhost:4567/index.html', as_html }
+    _then {
+      res.code.should eq '200'
+    }
+  end
 end
 
-describe "Login" do
-  _given {
-    headers "referer" => "http://local.example.com",
-            "user_agent" => "itesttool"
-  }
+describe 'POST form data' do
   _when {
-    post "http://localhost:4567/login",
-         body_as_form("nickname" => "admin",
-                      "password" => "pass"),
+    post 'http://localhost:4567/login',
+         body_as_form('nickname' => 'admin',
+                      'password' => 'pass'),
          res_is_json
   }
   _then {
-    res.code.should eq "200"
-    res["$.nickname"].should eq ["admin"]
+    res.code.should eq '200'
+    res['$.nickname'].should eq ['admin']
   }
 end
 
-describe "Post Json" do
-  context 'use "body"' do
+describe 'POST json' do
+  context 'specified json by "body()"' do
     _when {
-      post "http://localhost:4567/echo",
+      post 'http://localhost:4567/echo',
            body('{"name":"Shiro","age":2}'),
            res_is_json
     }
     _then {
-      res.code.should eq "200"
+      res.code.should eq '200'
       res.body.should eq '{"name":"Shiro","age":2}'
     }
   end
-  context 'use "body_as_json"' do
+  context 'specified json by "body_as_json()"' do
     _when {
-      post "http://localhost:4567/echo",
-           body_as_json("name" => "Shiro",
-                        "age" => 2),
+      post 'http://localhost:4567/echo',
+           body_as_json('name' => 'Shiro',
+                        'age' => 2),
            res_is_json
     }
     _then {
-      res.code.should eq "200"
+      res.code.should eq '200'
       res.body.should eq '{"name":"Shiro","age":2}'
     }
   end
+end
+
+describe 'Status 200 check' do
+  status_check [
+      'http://localhost:4567/index.json',
+      'http://localhost:4567/index'
+  ]
 end
 
